8241d903900bd8bfd1af7f35159af8ecdf4bea0a
Shwetha25git <shwetha@capbpm.com>
2025-07-09 14:19:09 +0530

    commiting first two task in training

---
+++ employeeManagement/src/PascalTriangle.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i =0; i <rows; i++) {
+
+            for(int j = 0; j <rows-i; j++) {
+                System.out.print(" ");
+            }
+            int number=1;
+            for (int k = 0; k <= i; k++) {
+                System.out.print(number + " ");
+                number = number * (i - k) / (k + 1);
+            }
+            System.out.println();
+        }
+    }
+}
