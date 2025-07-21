70cce7d0bba350dd6b674f162b8fde4d08a43dba
Shwetha25git <shwetha@capbpm.com>
2025-07-21 11:25:23 +0530

    code changes done for person

--- MavenTask/src/main/java/Day10Task/Person.java
+++ MavenTask/src/main/java/Day10Task/Person.java
@@ -9,8 +9,6 @@ import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 @JacksonXmlRootElement(localName = "person")
 public class Person {
     @JacksonXmlProperty(localName = "name")
