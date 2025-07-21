4514148908cb6ad3798ee7d4493f440d0e441c51
Shwetha25git <shwetha@capbpm.com>
2025-07-21 11:26:58 +0530

    change

--- MavenTask/src/main/java/Day10Task/Person.java
+++ MavenTask/src/main/java/Day10Task/Person.java
@@ -3,9 +3,8 @@ package Day10Task;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
